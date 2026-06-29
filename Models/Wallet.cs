0534caacd6fbe3b40420c9df1c8666be27741b18
kiranmakkineni <kirankmakkineni@gmail.com>
2026-06-29 22:07:50 +0530

    Add project structure and wallet entity

---
+++ Models/Wallet.cs
@@ -0,0 +1,12 @@
+﻿namespace GameEconomy.API.Models;
+
+public class Wallet
+{
+    public Guid Id { get; set; }
+
+    public string PlayerId { get; set; } = string.Empty;
+
+    public long Balance { get; set; }
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
